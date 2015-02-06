c688cfeeed5579eda29195e553ed55f3689b71fd
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2015-02-06 19:32:54 +0800

    add datepicker, not complete

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,7 +119,9 @@ module.exports = function (grunt) {
           'js/popover.js',
           'js/scrollspy.js',
           'js/tab.js',
-          'js/affix.js'
+          'js/affix.js',
+          'js/datepicker.js',
+          'js/timepicker.js'
         ],
         dest: 'dist/js/<%= pkg.name %>.js'
       }
