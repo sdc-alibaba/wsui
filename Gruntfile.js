b5dfe3633424949ab7f5bea91d74a5efaea93a9e
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2015-02-07 11:32:46 +0800

    add ac

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,6 +119,7 @@ module.exports = function (grunt) {
           'js/popover.js',
           'js/scrollspy.js',
           'js/tab.js',
+          'js/autocomplete.js',
           'js/affix.js'
         ],
         dest: 'dist/js/<%= pkg.name %>.js'
