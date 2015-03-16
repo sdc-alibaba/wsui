3c088d0fd6fb3d2dbca54f2f3a68fbfd0072e367
半边 <zangtao.zt@alibaba-inc.com>
2015-03-16 16:00:33 +0800

    _gh_pages 加入git管理，方便生成线上地址

---
+++ _gh_pages/assets/js/src/application.js
@@ -0,0 +1,207 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+/*!
+ * JavaScript for Bootstrap's docs (http://getbootstrap.com)
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+ * details, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+/* global ZeroClipboard */
+//jscs:disable 
+
+!function ($) {
+  'use strict';
+
+  $(function () {
+
+    // Scrollspy
+    var $window = $(window)
+    var $body   = $(document.body)
+
+    $body.scrollspy({
+      target: '.bs-docs-sidebar'
+    })
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    })
+
+    // Kill links
+    $('.bs-docs-container [href=#]').click(function (e) {
+      e.preventDefault()
+    })
+
+    // Sidenav affixing
+    setTimeout(function () {
+      var $sideBar = $('.bs-docs-sidebar')
+
+      $sideBar.affix({
+        offset: {
+          top: function () {
+            var offsetTop      = $sideBar.offset().top
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+            var navOuterHeight = $('.bs-docs-nav').height()
+
+            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+          },
+          bottom: function () {
+            return (this.bottom = $('.bs-docs-footer').outerHeight(true))
+          }
+        }
+      })
+    }, 100)
+
+    setTimeout(function () {
+      $('.bs-top').affix()
+    }, 100)
+
+    // theme toggler
+    ;(function () {
+      var stylesheetLink = $('#bs-theme-stylesheet')
+      var themeBtn = $('.bs-docs-theme-toggle')
+
+      var activateTheme = function () {
+        stylesheetLink.attr('href', stylesheetLink.attr('data-href'))
+        themeBtn.text('Disable theme preview')
+        localStorage.setItem('previewTheme', true)
+      }
+
+      if (localStorage.getItem('previewTheme')) {
+        activateTheme()
+      }
+
+      themeBtn.click(function () {
+        var href = stylesheetLink.attr('href')
+        if (!href || href.indexOf('data') === 0) {
+          activateTheme()
+        } else {
+          stylesheetLink.attr('href', '')
+          themeBtn.text('Preview theme')
+          localStorage.removeItem('previewTheme')
+        }
+      })
+    })();
+
+    // Tooltip and popover demos
+
+    // Button state demo
+    $('#loading-example-btn').on('click', function () {
+      var btn = $(this)
+      btn.button('loading')
+      setTimeout(function () {
+        btn.button('reset')
+      }, 3000)
+    })
+
+    // Modal relatedTarget demo
+    $('#exampleModal').on('show', function (event) {
+      var button = $(event.relatedTarget) // Button that triggered the modal
+      var recipient = button.data('whatever') // Extract info from data-* attributes
+      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+      var modal = $(this)
+      modal.find('.modal-title').text('New message to ' + recipient)
+      modal.find('.modal-body input').val(recipient)
+    })
+
+    // Activate animated progress bar
+    $('.bs-docs-activate-animated-progressbar').on('click', function () {
+      $(this).siblings('.progress').find('.progress-bar-striped').toggleClass('active')
+    })
+
+    //
+    $('#datepicker-input-js').datepicker();
+    $('#datepicker-inline-1').datepicker();
+
+
+    // ac
+    $('#ac-js-input').autocomplete({
+      serviceUrl: '../json/ac-strings.json'
+    });
+    //intro
+    $(".btn-intro").click(function(){
+      $.introJs().start();
+    })
+
+    // Config ZeroClipboard
+    ZeroClipboard.config({
+      moviePath: '/assets/flash/ZeroClipboard.swf',
+      hoverClass: 'btn-clipboard-hover'
+    })
+
+    // Insert copy to clipboard button before .highlight
+    $('.highlight').each(function () {
+      var btnHtml = '<div class="zero-clipboard"><span class="btn-clipboard">Copy</span></div>'
+      $(this).before(btnHtml)
+    })
+    var zeroClipboard = new ZeroClipboard($('.btn-clipboard'))
+    var htmlBridge = $('#global-zeroclipboard-html-bridge')
+
+    // Handlers for ZeroClipboard
+    zeroClipboard.on('load', function () {
+      htmlBridge
+        .data('placement', 'top')
+        .attr('title', 'Copy to clipboard')
+        .tooltip()
+    })
+
+    // Copy to clipboard
+    zeroClipboard.on('dataRequested', function (client) {
+      var highlight = $(this).parent().nextAll('.highlight').first()
+      client.setText(highlight.text())
+    })
+
+    // Notify copy success and reset tooltip title
+    zeroClipboard.on('complete', function () {
+      htmlBridge
+        .attr('title', 'Copied!')
+        .tooltip('fixTitle')
+        .tooltip('show')
+        .attr('title', 'Copy to clipboard')
+        .tooltip('fixTitle')
+    })
+
+    // Notify copy failure
+    zeroClipboard.on('noflash wrongflash', function () {
+      htmlBridge
+        .attr('title', 'Flash required')
+        .tooltip('fixTitle')
+        .tooltip('show')
+    })
+
+    //toast
+    $('#toast-topleft').click(function(event){
+      event.preventDefault();
+      $.toast({text:'button topleft danger',type:'danger',position:'top left'});
+    });
+    $('#toast-top').click(function(event){
+      event.preventDefault();
+      $.toast({text:'button top success',posititon:'top',type:'success'});
+    });
+    $('#toast-topright').click(function(event){
+      event.preventDefault();
+      $.toast({text:'button topright warning',type:'warning',position:'top right'});
+    });
+    $('#toast-center').click(function(event){
+      event.preventDefault();
+      $.toast({text:'button center danger',type:'danger',position:'center'});
+    });
+    $('#toast-bottomleft').click(function(event){
+      event.preventDefault();
+      $.toast({text:'button bottomleft danger',type:'danger',position:'bottom left'});
+    });
+    $('#toast-bottom').click(function(event){
+      event.preventDefault();
+      $.toast({text:'button bottom success',type:'success',position:'bottom'});
+    });
+    $('#toast-bottomright').click(function(event){
+      event.preventDefault();
+      $.toast('button bottomright warning','warning','bottom right');
+    });
+
+
+  })
+
+}(jQuery)
