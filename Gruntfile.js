eecfc148ea7bcfa66527135a76f0693f237866d2
半边 <zangtao.zt@alibaba-inc.com>
2015-04-01 16:42:45 +0800

    update li and tags

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
     'js/alert.js',
     'js/button.js',
     'js/carousel.js',
-    // 'js/collapse.js',
+    'js/collapse.js',
     'js/dropdown.js',
     'js/modal.js',
     'js/tooltip.js',
