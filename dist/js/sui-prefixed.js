f9c3d2d67584066dde084379efda4568f9aa027e
慕扉 <fumeng.fm@alibaba-inc.com>
2015-04-03 17:20:08 +0800

    review addoncount

--- dist/js/sui-prefixed.js
+++ dist/js/sui-prefixed.js
@@ -2234,19 +2234,12 @@ window.CLASSMAP = {
 
   'use strict';
   // document ready init
-  $(function () {
-    $(document).on('input propertychange', '[data-max]', function () {
-      var that = this,
-          len = $(that).data('max'),
-          count = $(that).val().length;
-      if (count < len) {
-        $(that).siblings('.input-group-addon-count').text(count + '/' + len);
-      } else {
-        $(that).siblings('.input-group-addon-count').html('<span style="color:#f23f40">' + count + '/' + len + '</span>');
-      }
-    });
-  })
-
+  $(document).on('input propertychange', '[data-max]', function () {
+    var that = this,
+        len = $(that).data('max'),
+        count = $(that).val().length;
+    $(that).siblings('.input-group-addon-count').html('<span class="' + (count < len ? '' : 'text-primary') + '">' + count + '/' + len + '</span>');
+  });
 }(jQuery);
 
 !function ($) {
--- js/addoncount.js
+++ js/addoncount.js
@@ -3,17 +3,10 @@
 
   'use strict';
   // document ready init
-  $(function () {
-    $(document).on('input propertychange', '[data-max]', function () {
-      var that = this,
-          len = $(that).data('max'),
-          count = $(that).val().length;
-      if (count < len) {
-        $(that).siblings('.input-group-addon-count').text(count + '/' + len);
-      } else {
-        $(that).siblings('.input-group-addon-count').html('<span style="color:#f23f40">' + count + '/' + len + '</span>');
-      }
-    });
-  })
-
+  $(document).on('input propertychange', '[data-max]', function () {
+    var that = this,
+        len = $(that).data('max'),
+        count = $(that).val().length;
+    $(that).siblings('.input-group-addon-count').html('<span class="' + (count < len ? '' : 'text-primary') + '">' + count + '/' + len + '</span>');
+  });
 }(jQuery);
