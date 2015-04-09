97484b5196ea5113cac04faba34f27ed98edef2f
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2015-04-09 14:28:30 +0800

    fix jscs error

--- js/tag.js
+++ js/tag.js
@@ -1,18 +1,18 @@
 +function ($) {
   'use strict';
 
-  $(document).on("change", "[data-toggle='tag'] input[type]", function(e){
+  $(document).on('change', '[data-toggle="tag"] input[type]', function (e) {
     var $input = $(e.target);
-    var $group = $input.parents("."+CLASSMAP.tagGroup);
-    $group.find("input[type]").each(function() {
+    var $group = $input.parents('.' + CLASSMAP.tagGroup);
+    $group.find('input[type]').each(function () {
       var $this = $(this);
-      var checked = $this.is(":checked");
-      $this.parents(".tag")[checked ? "addClass" : "removeClass"]("active");
+      var checked = $this.is(':checked');
+      $this.parents('.tag')[checked ? 'addClass' : 'removeClass']('active');
     });
   })
-  $(document).on("click", "[data-toggle='tag'] [name='remove']", function(e){
+  $(document).on('click', '[data-toggle="tag"] [name="remove"]', function (e) {
     var $btn = $(e.target);
-    $btn.parents(".tag").remove();
+    $btn.parents('.tag').remove();
   })
 
 }(jQuery);
